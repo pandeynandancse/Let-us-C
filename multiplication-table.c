b1283b893e1665ef319b7fa00e75ac2afc4dc4d6
Nandan Pandey <pandeynandancse@gmail.com>
2018-10-16 11:15:50 -0700

    Update multiplication-table.c

--- multiplication-table.c
+++ multiplication-table.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 int main(void) {
+	// your code goes here
 	int a,b,c,d;
 	printf("Enter the number whose multiplication table is to be found: ");
 	scanf("%d",&a);
-	printf("Enter the number from where your multication must be started: ");
+	printf("\n");
+	printf("Enter the number from where your multiplication must be started: ");
 	scanf("%d",&d);
+	printf("\n");
 	printf("Enter the number till which you have to find table: ");
 	scanf("%d",&b);
+	printf("\n");
 	if(d<b){
 		for(int i=d;i<=b;i++){
 			c = a*i;
@@ -14,6 +18,8 @@ int main(void) {
 			printf("\n");
 		}
 	}
-	
+	else{
+		printf("Error");
+	}
 	return 0;
 }
