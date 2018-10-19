f03133019170828677b9fde8feb4bd042e6eafca
Nandan Pandey <pandeynandancse@gmail.com>
2018-10-19 03:59:27 -0700

    Update floyd-triangle.c

--- patterns/floyd-triangle.c
+++ patterns/floyd-triangle.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
+int main(void) {
+	int i,j,a,num=0;
+	printf("Enter the number of rows of floyd's triangle : ");
+	scanf("%d",&a);
+	printf("\n");
+	for(i=0;i<a;i++){
+		for(j=0;j<=i;j++){
+			num++;
+			printf("%d",num);
+			printf(" ");
+		}
+		printf("\n");
+	}
+	return 0;
+}
 
