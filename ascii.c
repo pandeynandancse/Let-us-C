30362cec138b11e21c84126785cd054b443e0b8e
Nandan Pandey <pandeynandancse@gmail.com>
2018-10-20 22:11:00 +0530

    Create ascii.c

---
+++ ascii.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void) {
+	char a;
+	int x;
+	printf("Enter character whose ascii value has to be found : ");
+	scanf("%c",&a);
+	printf("\n");
+	x = a;
+	printf("%d",x);
+	return 0;
+}
