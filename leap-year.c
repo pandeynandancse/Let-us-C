b6a77b88b0e7f4ad963aea43cec68eb3cf876a9d
Nandan Pandey <pandeynandancse@gmail.com>
2018-10-20 21:14:21 +0530

    Update leap-year.c

--- leap-year.c
+++ leap-year.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+int main(void) {
+	int a;
+	printf("Enter year to be checked");
+	scanf("%d",&a);
+	printf("\n");
+	if(a % 4 == 0){
+		if(a % 100 == 0){
+			if(a % 400 == 0){
+				printf("%d is a leap year",a);
+			}
+			else{
+				printf("%d is not a leap year",a);
+			}
+		}
+		else{
+				printf("%d is a leap year",a);
+		}
+	}
+	else{
+		printf("%d is not a leap year",a);
+	}
+	return 0;
+}
 
