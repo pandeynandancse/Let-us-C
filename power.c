86c20776b565239158c6a50b32e84d05e95fb823
Nandan Pandey <pandeynandancse@gmail.com>
2018-10-23 19:36:25 +0530

    Update power.c

--- power.c
+++ power.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 int main(void) {
 	int i,a,b,po=1;
+	printf("Enter the number(BASE): ");
 	scanf("%d",&a);
+	printf("\n");
+	printf("Enter the power : ");
 	scanf("%d",&b);
+	printf("\n");
 	for(i=1;i<=b;i++){
 		po = po * a;
 	}
-	printf("%d",po);
+	printf("Power of given number is : %d",po);
 	return 0;
 }
 
