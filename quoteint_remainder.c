0673728b300eb95801084f56a9133334fecd1372
Nandan Pandey <pandeynandancse@gmail.com>
2018-10-23 19:42:31 +0530

    Update quoteint_remainder.c

--- quoteint_remainder.c
+++ quoteint_remainder.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+int main(void) {
+	int a,b,c,d;
+	printf("Enter the Dividend : ");
+	scanf("%d",&a);
+	printf("\n");
+	printf("Enter the Divisor : ");
+	scanf("%d",&b);
+	printf("\n");
+	c = a/b;
+	d = a%b;
+	printf("Quotient is : %d",c);
+	printf("\n");
+	printf("Remainder is : %d",d);
+	return 0;
+}
+
 
