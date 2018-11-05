6ce9e694b86476ffc9534f7ac0ff8bd21aff3c5a
Nandan Pandey <pandeynandancse@gmail.com>
2018-11-06 01:07:09 +0530

    Update decimal_to_binary.c

--- decimal_to_binary.c
+++ decimal_to_binary.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+#include<math.h>
+int main(void) {
+	// your code goes here
+	int i=0,a,b=0,no;
+	printf("Enter the number : ");
+	scanf("%d",&no);
+	while(no>0)
+	{
+		a = no%2;
+		no = no/2;
+		b = b + a*pow(10,i);
+		i++;
+		
+	}
+	printf("%d",b);
+	return 0;
+}
 
